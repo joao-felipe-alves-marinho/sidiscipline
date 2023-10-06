12d5ca21f75f71fd669402da9a7f8178219aaaca
Joao Felipe <joao.felipe.alves.m@gmail.com>
2023-10-06 00:50:59 -0300

    Temporario: Validação de login

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,13 +1,15 @@
-import { Avatar, Box, Divider, Drawer, Typography } from "@mui/material";
+import { Avatar, Box, Divider, Drawer, List, ListItem, ListItemButton, ListItemText, Typography } from "@mui/material";
 import { grey } from "@mui/material/colors";
 
 // eslint-disable-next-line react-hooks/rules-of-hooks
 
 function Menu() {
     return (
-        <Drawer variant="permanent" anchor="left" PaperProps={{sx: {
-            bgcolor: grey['300'],
-        }}}>
+        <Drawer variant="permanent" anchor="left" PaperProps={{
+            sx: {
+                bgcolor: grey['300'],
+            }
+        }}>
             <Box width={224} height='100%' display='flex' flexDirection='column'>
                 <Box display='flex' alignContent='center' justifyContent='center'>
                     <Avatar sx={{ margin: '25px', width: '60px', height: '60px', bgcolor: "secondary.main" }}>D</Avatar>
@@ -16,7 +18,19 @@ function Menu() {
                 <Divider variant="middle" />
 
                 <Box flex={1}>
-
+                    <List>
+                        <ListItem disablePadding>
+                            <ListItemButton autoFocus>
+                                <ListItemText primary="Home" />
+                            </ListItemButton>
+                        </ListItem>
+                        <Divider />
+                        <ListItem disablePadding>
+                            <ListItemButton href="/login">
+                                <ListItemText primary="Log out" />
+                            </ListItemButton>
+                        </ListItem>
+                    </List>
                 </Box>
 
             </Box>
@@ -29,11 +43,11 @@ function Home() {
         <>
             <Menu></Menu>
             <Box height='100vh' sx={{ bgcolor: grey['200'] }} display='flex' flexDirection='column'>
-                <Box boxShadow={1} display='flex' alignContent='end' justifyContent='right' sx={{bgcolor: 'primary.light'}}>
-                    <Typography color={"white"} marginX={1} variant="h2"> Dashboard</Typography>
+                <Box boxShadow={1} display='flex' alignContent='end' justifyContent='right' sx={{ bgcolor: 'primary.main' }}>
+                    <Typography color={"white"} fontWeight={'bold'} marginX={1} variant="h2">SIDISCIPLINE</Typography>
                 </Box>
                 <Box margin={1} flex={1} display='flex' alignContent='center' justifyContent='center'>
-                    <Typography variant="h3"> Box corpo</Typography>
+                    <Typography variant="h3"> WIP </Typography>
                 </Box>
             </Box>
         </>
--- src/app/pages/Login.tsx
+++ src/app/pages/Login.tsx
@@ -1,6 +1,38 @@
-import { Box, Button, Card, CardActions, CardContent, Checkbox, FormControlLabel, Link, TextField, Typography } from '@mui/material';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Box, Button, Card, CardActions, CardContent, Checkbox, FormControlLabel, Link, Stack, TextField, Typography } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router';
+import * as yup from 'yup';
+
+interface FormValues {
+    email: string;
+    password: string;
+}
+
+const schema = yup.object({
+    email: yup.string().required("Preencha o campo de E-mail.").email("Insira um e-mail válido."),
+    password: yup.string().required("Preencha o campo de Senha.")
+})
 
 function Login() {
+
+    const { register, handleSubmit, formState } = useForm<FormValues>({
+        defaultValues: {
+            email: '',
+            password: '',
+        },
+        resolver: yupResolver(schema)
+    })
+
+    const { errors } = formState;
+
+    const navigate = useNavigate();
+
+    const onSubmit = (data: FormValues) =>{
+        console.log(data);
+        navigate('/home');
+    }
+
     return (
         <>
             <Box component='div' sx={{
@@ -10,41 +42,54 @@ function Login() {
             }}>
                 <Typography fontWeight='bold' variant='h3' color={'white'} padding={1}>Sidiscipline</Typography>
                 <Box width='100vw' height='75vh' display='flex' alignItems='center' justifyContent='center'>
-                    <Card  sx={{
-                    border: 1,
-                    borderColor: 'secondary.main',
-                    boxShadow: 3,
-                    width: '700px',
-                    paddingX: 5,
-                    paddingY: 2
-                }}>
-                        <CardContent>
-                            <Box display='flex' flexDirection='column' gap={2} >
-                                <Typography fontWeight='600' variant='h2' align='center' color='primary'> Login </Typography>
+                    <Card sx={{
+                        border: 1,
+                        borderColor: 'secondary.main',
+                        borderRadius: 5,
+                        boxShadow: 3,
+                        width: '700px',
+                        paddingX: 5,
+                        paddingY: 2
+                    }}>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <CardContent>
+                                <Stack display='flex' flexDirection='column' gap={2} >
+                                    <Typography fontWeight='600' variant='h2' align='center' color='primary'> Login </Typography>
 
-                                <TextField
-                                    label='Email'
-                                    type='email' />
+                                    <TextField
+                                        fullWidth
+                                        color='secondary'
+                                        label='Email'
+                                        type='email' 
+                                        {...register("email")}
+                                        error={!!errors.email}
+                                        helperText={errors.email?.message} />
+                                        
+                                    <TextField
+                                        fullWidth
+                                        color='secondary'
+                                        label='Senha'
+                                        type='password'
+                                        {...register("password")}
+                                        error={!!errors.password}
+                                        helperText={errors.password?.message} />
 
-                                <TextField
-                                    label='Senha'
-                                    type='password' />
-
-                                <FormControlLabel control={<Checkbox color='secondary' />} label="Lembre-se de mim" />
-                            </Box>
-                        </CardContent>
-                        <CardActions>
-                            <Button
-                            sx={{
-                                fontWeight: 'bold'
-                            }}
-                                variant='contained'
-                                href='/home'
-                                color='secondary'
-                                fullWidth>
-                                Login
-                            </Button>
-                        </CardActions>
+                                    <FormControlLabel control={<Checkbox color='secondary' />} label="Lembre-se de mim" />
+                                </Stack>
+                            </CardContent>
+                            <CardActions>
+                                <Button
+                                    type='submit'
+                                    sx={{
+                                        fontWeight: 'bold'
+                                    }}
+                                    variant='contained'
+                                    color='secondary'
+                                    fullWidth>
+                                    Login
+                                </Button>
+                            </CardActions>
+                        </form>
                         <CardContent>
                             <Box display='flex' flexDirection='row' justifyContent='space-between'>
                                 <Link variant='body1'>Cadastre-se</Link>
